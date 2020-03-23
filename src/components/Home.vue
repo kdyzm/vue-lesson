a7ee5ab110bf51c7e6291050754cfe73b235d05c
kdyzm <kdyzm@foxmail.com>
2020-03-23 21:09:41 +0800

    vue-resource渲染数据

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,11 @@
     <div>
         <h2>{{title}}</h2>
         <button @click="getData()">请求数据</button>
+        <ul>
+            <li v-for="(item,key) in list.data" :key="key">
+                {{item}}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -10,13 +15,15 @@
         name: "Home",
         data() {
             return {
-                title: "我是Home组件"
+                title: "我是Home组件",
+                list: []
             }
         }, methods: {
             getData() {
-                var url="http://rap2.taobao.org:38080/app/mock/246551/react-demo/news-list";
+                var url = "http://rap2.taobao.org:38080/app/mock/246551/react-demo/news-list";
                 this.$http.get(url).then((res) => {
                     console.log(res.data);
+                    this.list = res.data;
                 }, (err) => {
                     console.log(err);
                 })
