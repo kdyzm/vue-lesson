3819ea60fe7e3d346cb6dcebd444c13e0f912cc5
kdyzm <kdyzm@foxmail.com>
2020-03-25 22:18:02 +0800

    路由嵌套、使用scss

--- src/main.js
+++ src/main.js
@@ -9,21 +9,38 @@ import VueRouter from 'vue-router'
 import Home from './components/Home';
 import News from './components/News';
 import NewsDetail from './components/NewsDetail';
+import User from './components/User';
+import UserAdd from './components/user/UserAdd';
+import UserList from './components/user/UserList';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
 //2.定义路由
 const routes = [
-    { path: '/home', component: Home },
-    { path: '/news', component: News },
-    { path: '/news/detail/:aid', component: NewsDetail },
-    { path: '/news/detail', component: NewsDetail },
-    { path: '*', redirect: '/home' }
+    {path: '/home', component: Home},
+    {path: '/news', component: News},
+    {path: '/news/detail/:aid', component: NewsDetail},
+    {path: '/news/detail', component: NewsDetail},
+    {
+        path: '/user',
+        component: User,
+        children: [
+            {
+                path: 'add',
+                component: UserAdd
+            },
+            {
+                path: 'list',
+                component: UserList
+            }
+        ]
+    },
+    {path: '*', redirect: '/home'}
 ]
 
 //3.创建router实例，传routes配置
-const router=new VueRouter({
+const router = new VueRouter({
     routes
 })
 
